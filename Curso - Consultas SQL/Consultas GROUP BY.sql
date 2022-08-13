4c2ca4e8c7bcf9bcb7bbc7b1110fc53d6a959763
Gustavo Faria Morette <88351614+Morettegustavo@users.noreply.github.com>
2022-08-13 10:47:06 -0300

    Update Consultas GROUP BY.sql

--- Curso - Consultas SQL/Consultas GROUP BY.sql
+++ Curso - Consultas SQL/Consultas GROUP BY.sql
@@ -23,3 +23,9 @@ WHERE CIDADE = 'rio de janeiro'
 GROUP BY ESTADO, BAIRRO
 ORDER BY BAIRRO;
 
+# Aproveitando o exercício do vídeo anterior quantos itens de venda existem com a maior quantidade do produto '1101035'?
+SELECT MAX(`QUANTIDADE`) as 'MAIOR QUANTIDADE' FROM itens_notas_fiscais WHERE `CODIGO_DO_PRODUTO` = '1101035';
+SELECT COUNT('QUANTIDADE') as 'MAIOR QUANTIDADE' FROM itens_notas_fiscais WHERE CODIGO_DO_PRODUTO = '1101035' AND QUANTIDADE = 99;
+
+
+
