11d4c319a607663279a16b1053ae77f7f3ea940f
Gustavo Faria Morette <88351614+Morettegustavo@users.noreply.github.com>
2022-10-04 08:45:04 -0300

    chore:add_left_and_right_join

---
+++ Curso - Consultas SQL/Consultas LEFT e RIGHT JOIN.sql
@@ -0,0 +1,29 @@
+use sucos_vendas;
+
+SELECT COUNT(*) FROM tabela_de_clientes;
+
+SELECT CPF, COUNT(*) FROM notas_fiscais
+GROUP BY CPF;
+
+SELECT DISTINCT A.CPF, A.NOME, B.CPF FROM tabela_de_clientes A 
+INNER JOIN notas_fiscais B 
+ON A.CPF = B.CPF;
+
+/* LEFT JOIN */
+
+SELECT DISTINCT A.CPF, A.NOME, B.CPF FROM tabela_de_clientes A 
+LEFT JOIN notas_fiscais B 
+ON A.CPF = B.CPF
+WHERE B.CPF IS NULL;
+
+SELECT DISTINCT A.CPF, A.NOME, B.CPF FROM tabela_de_clientes A 
+LEFT JOIN notas_fiscais B 
+ON A.CPF = B.CPF
+WHERE B.CPF IS NULL AND YEAR(B.DATA_VENDA) = 2015;
+
+/* RIGTH JOIN */
+
+SELECT DISTINCT A.CPF, A.NOME, B.CPF FROM notas_fiscais B 
+RIGHT JOIN tabela_de_clientes A 
+ON A.CPF = B.CPF
+WHERE B.CPF IS NULL;
