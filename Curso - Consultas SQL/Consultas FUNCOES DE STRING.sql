2c61c2d041a2dc9e99d8806d6fa94d5db675625a
Gustavo Faria Morette <88351614+Morettegustavo@users.noreply.github.com>
2022-10-08 12:53:25 -0300

    chore:add-ex

--- Curso - Consultas SQL/Consultas FUNCOES DE STRING.sql
+++ Curso - Consultas SQL/Consultas FUNCOES DE STRING.sql
@@ -21,3 +21,8 @@ SELECT SUBSTRING('OLÁ, TUDO BEM?', 6) AS RESULTADO;
 SELECT SUBSTRING('OLÁ, TUDO BEM?', 6, 4) AS RESULTADO;
 
 SELECT CONCAT(NOME, ' (', CPF, ') ') AS RESULTADO FROM tabela_de_clientes;
+
+/* Faça uma consulta listando o nome do cliente e o endereço completo (Com rua, bairro, cidade e estado). */
+
+SELECT NOME, CONCAT(ENDERECO_1, ' ', BAIRRO, ' ', CIDADE, ' ', ESTADO) AS COMPLETO
+FROM tabela_de_clientes
